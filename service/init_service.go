6f7f70a4f90933b9c7acb15a14ea00c4ae20b410
Kevin <kevin_ching@paradise-soft.com.tw>
2020-05-12 17:46:19 +0800

    update version

--- service/init_service.go
+++ service/init_service.go
@@ -5,7 +5,7 @@ import (
 	"gitlab.paradise-soft.com.tw/glob/dispatcher/model"
 )
 
-var _ver = "v1.13.3"
+var _ver = "v1.14.0"
 
 func Init(brokers []string, opts []model.Option) error {
 	dis := model.MakeDispatcher(opts)
